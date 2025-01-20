bcc1a0661361ae2787dd8e266006f26219aef6fb
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-20 17:56:44 -0600

    Put old config in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,14 @@
 import NextAuth from 'next-auth'
-import { authConfig } from '@/auth.config'
 import Cognito from 'next-auth/providers/cognito'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    ...authConfig,
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        authorized: async ({ auth }) => {
+            return !!auth
+        }
+    },
     providers: [Cognito]
 })
