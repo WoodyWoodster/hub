0f721281f4a81f9845968fe2e9e0ff206c4138ac
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-23 11:06:31 -0600

    Added addresses table and created relationships between it and people

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,33 @@
+import { relations } from 'drizzle-orm';
 import { date, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
 export const people = pgTable('people', {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	fullName: varchar('full_name', { length: 255 }).notNull(),
-	email: varchar({ length: 255 }).notNull().unique(),
+	email: varchar('email', { length: 255 }).notNull().unique(),
 	dateOfBirth: date('date_of_birth').notNull(),
 	hireDate: date('hire_date').notNull(),
 });
+
+export const addresses = pgTable('addresses', {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	personId: integer('person_id').notNull(),
+	streetAddress: varchar('street_address', { length: 255 }).notNull(),
+	city: varchar('city', { length: 100 }).notNull(),
+	state: varchar('state', { length: 2 }).notNull(),
+	zipCode: varchar('zip_code', { length: 10 }).notNull(),
+});
+
+export const peopleRelations = relations(people, ({ one }) => ({
+	address: one(addresses, {
+		fields: [people.id],
+		references: [addresses.personId],
+	}),
+}));
+
+export const addressesRelations = relations(addresses, ({ one }) => ({
+	person: one(people, {
+		fields: [addresses.personId],
+		references: [people.id],
+	}),
+}));
