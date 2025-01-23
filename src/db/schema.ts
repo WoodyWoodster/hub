dfccd1a8b6c1d67cf438f66f95b23a22435870b7
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-23 11:19:01 -0600

    Added snake_case to db config

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,19 +3,19 @@ import { date, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
 export const people = pgTable('people', {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	fullName: varchar('full_name', { length: 255 }).notNull(),
-	email: varchar('email', { length: 255 }).notNull().unique(),
-	dateOfBirth: date('date_of_birth').notNull(),
-	hireDate: date('hire_date').notNull(),
+	fullName: varchar({ length: 255 }).notNull(),
+	email: varchar({ length: 255 }).notNull().unique(),
+	dateOfBirth: date().notNull(),
+	hireDate: date().notNull(),
 });
 
 export const addresses = pgTable('addresses', {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	personId: integer('person_id').notNull(),
-	streetAddress: varchar('street_address', { length: 255 }).notNull(),
-	city: varchar('city', { length: 100 }).notNull(),
-	state: varchar('state', { length: 2 }).notNull(),
-	zipCode: varchar('zip_code', { length: 10 }).notNull(),
+	personId: integer().notNull(),
+	streetAddress: varchar({ length: 255 }).notNull(),
+	city: varchar({ length: 100 }).notNull(),
+	state: varchar({ length: 2 }).notNull(),
+	zipCode: varchar({ length: 10 }).notNull(),
 });
 
 export const peopleRelations = relations(people, ({ one }) => ({
