d0bb6255015ccd92e27f2ca09869b31d83dadce6
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-23 19:01:27 -0600

    Added logs to troubleshoot prod

--- src/app/(application)/people/page.tsx
+++ src/app/(application)/people/page.tsx
@@ -3,6 +3,8 @@ import { db } from '@/db';
 import { people } from '@/db/schema';
 
 export default async function PeoplePage() {
+	console.log('Getting people');
 	const allPeople = await db.select().from(people);
+	console.log(allPeople);
 	return <PeopleTable people={allPeople} />;
 }
