9cd169681b7099e67d2f0ea5d7040e29008fe22f
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-25 15:09:51 -0600

    Created companies, companyPeople and companyAddreses

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,16 +5,39 @@ export const people = pgTable('people', {
 	fullName: varchar('full_name', { length: 255 }).notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
 	dateOfBirth: date('date_of_birth').notNull(),
-	hireDate: date('hire_date').notNull(),
 });
 
 export const addresses = pgTable('addresses', {
 	id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-	personId: integer('person_id')
-		.references(() => people.id)
-		.notNull(),
 	streetAddress: varchar('street_address', { length: 255 }).notNull(),
 	city: varchar({ length: 100 }).notNull(),
 	state: varchar({ length: 2 }).notNull(),
 	zipCode: varchar('zip_code', { length: 10 }).notNull(),
+});
+
+export const companies = pgTable('companies', {
+	id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar({ length: 255 }).notNull(),
+	website: varchar({ length: 255 }).notNull(),
+	industry: varchar({ length: 50 }).notNull(),
+	size: varchar({ length: 50 }).notNull(),
+});
+
+export const companyPeople = pgTable('company_people', {
+	companyId: integer('company_id')
+		.references(() => companies.id)
+		.notNull(),
+	personId: integer('person_id')
+		.references(() => people.id)
+		.notNull(),
+	hireDate: date('hire_date'),
+});
+
+export const companyAddresses = pgTable('company_addresses', {
+	companyId: integer('company_id')
+		.references(() => companies.id)
+		.notNull(),
+	addressId: integer('address_id')
+		.references(() => addresses.id)
+		.notNull(),
 });
