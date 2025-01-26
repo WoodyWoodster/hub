f53786079f494cd518be2f02427c6b6391735bcc
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-25 19:58:54 -0600

    Added check to sign-up to redirect authenticated users to dashboard

--- src/app/(onboarding)/sign-up/page.tsx
+++ src/app/(onboarding)/sign-up/page.tsx
@@ -1,7 +1,12 @@
-'use client';
-
+import { auth } from '@/auth';
 import { SignUpForm } from '@/components/custom/forms/onboarding/sign-up-form';
+import { redirect } from 'next/navigation';
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+	const session = await auth();
+
+	if (session) {
+		redirect('/dashboard');
+	}
 	return <SignUpForm />;
 }
