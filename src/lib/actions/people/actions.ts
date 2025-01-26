b34ad1f56567838f43dde5c5851899b1756e8a6e
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-25 19:57:15 -0600

    Swapped permanentRedirect for redirect

--- src/lib/actions/people/actions.ts
+++ src/lib/actions/people/actions.ts
@@ -5,7 +5,7 @@ import { db } from '@/db';
 import { addresses, people } from '@/db/schema';
 import { addPersonSchema } from '@/lib/schemas/people/add-person-schema';
 import { revalidateTag } from 'next/cache';
-import { permanentRedirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 export async function addPersonAction(_prevState: unknown, formData: FormData) {
@@ -88,5 +88,5 @@ export async function addPersonAction(_prevState: unknown, formData: FormData) {
 
 export async function logOut() {
 	await signOut();
-	permanentRedirect('/');
+	redirect('/login');
 }
