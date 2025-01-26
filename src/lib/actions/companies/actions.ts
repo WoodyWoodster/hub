ae9e780649a6501b9a845087a38f1c784b2e72e1
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-25 22:43:16 -0600

    Simplified signUpCompanyAction

--- src/lib/actions/companies/actions.ts
+++ src/lib/actions/companies/actions.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use server';
 
+import { createUser } from '@/auth';
 import { db } from '@/db';
 import {
 	addresses,
@@ -10,124 +10,79 @@ import {
 	people,
 } from '@/db/schema';
 import { signUpCompanySchema } from '@/lib/schemas/companies/sign-up-company-schema';
+import { isErr } from '@/types/result';
 
-function parseFormData(formData: FormData): Record<string, any> {
-	const obj: Record<string, any> = {};
-	formData.forEach((value, key) => {
-		const keys = key.split('.');
-		let current = obj;
-		keys.forEach((k, index) => {
-			if (index === keys.length - 1) {
-				current[k] = value;
-			} else {
-				current[k] = current[k] || {};
-				current = current[k];
-			}
-		});
+export async function signUpCompanyAction(formData: FormData) {
+	console.log('formData', formData);
+	const validatedFields = signUpCompanySchema.safeParse({
+		person: {
+			fullName: formData.get('person.fullName'),
+			email: formData.get('person.email'),
+			dateOfBirth: formData.get('person.dateOfBirth'),
+			password: formData.get('person.password'),
+			confirmPassword: formData.get('person.confirmPassword'),
+		},
+		company: {
+			name: formData.get('company.name'),
+			website: formData.get('company.website'),
+			industry: formData.get('company.industry'),
+			size: formData.get('company.size'),
+		},
+		address: {
+			street: formData.get('address.street'),
+			city: formData.get('address.city'),
+			state: formData.get('address.state'),
+			zipCode: formData.get('address.zipCode'),
+		},
 	});
-	console.log('Parsed Data:', JSON.stringify(obj, null, 2));
-	return obj;
-}
 
-export async function signUpCompanyAction(
-	_prevState: unknown,
-	formData: FormData,
-) {
+	if (!validatedFields.success) {
+		return { error: validatedFields.error.flatten().fieldErrors };
+	}
+
+	const { person, company, address } = validatedFields.data;
+
+	console.log('person', person);
+	console.log('company', company);
+	console.log('address', address);
+
 	try {
-		console.log('formData', formData);
-		const parsedData = parseFormData(formData);
-		const data = signUpCompanySchema.parse(parsedData);
-		console.log('Saving company data', data);
-
-		const insertCompanyData: typeof companies.$inferInsert = {
-			name: data.company.name,
-			size: data.company.size,
-			industry: data.company.industry,
-			website: data.company.website,
-		};
-
-		const insertPersonData: typeof people.$inferInsert = {
-			email: data.person.email,
-			fullName: data.person.fullName,
-			dateOfBirth: data.person.dateOfBirth,
-		};
-
-		const insertCompanyAddressData: typeof addresses.$inferInsert = {
-			streetAddress: data.address.street,
-			city: data.address.city,
-			state: data.address.state,
-			zipCode: data.address.zipCode,
-		};
-
 		await db.transaction(async (tx) => {
 			const [insertedCompany] = await tx
 				.insert(companies)
-				.values(insertCompanyData)
+				.values(company)
 				.returning({ id: companies.id });
 
 			const [insertedPerson] = await tx
 				.insert(people)
-				.values(insertPersonData)
+				.values(person)
 				.returning({ id: people.id });
 
 			const [insertedAddress] = await tx
 				.insert(addresses)
-				.values(insertCompanyAddressData)
+				.values(address)
 				.returning({ id: addresses.id });
 
-			await tx
-				.insert(companyPeople)
-				.values({
-					companyId: insertedCompany.id,
-					personId: insertedPerson.id,
-				})
-				.returning({
-					companyId: companyPeople.companyId,
-					personId: companyPeople.personId,
-				});
+			await tx.insert(companyPeople).values({
+				companyId: insertedCompany.id,
+				personId: insertedPerson.id,
+			});
 
-			await tx
-				.insert(companyAddresses)
-				.values({
-					companyId: insertedCompany.id,
-					addressId: insertedAddress.id,
-				})
-				.returning({
-					companyId: companyAddresses.companyId,
-					addressId: companyAddresses.addressId,
-				});
+			await tx.insert(companyAddresses).values({
+				companyId: insertedCompany.id,
+				addressId: insertedAddress.id,
+			});
 		});
 
-		return {
-			defaultValues: {
-				person: {
-					fullName: '',
-					email: '',
-					dateOfBirth: '',
-				},
-				company: {
-					name: '',
-					website: '',
-					industry: '',
-					size: '',
-				},
-				address: {
-					street: '',
-					city: '',
-					state: '',
-					zipCode: '',
-					country: '',
-				},
-			},
-			success: true,
-			errors: {} as Record<string, string>,
-		};
+		const result = await createUser(person.email, person.password);
+
+		if (isErr(result)) {
+			return { error: result.error };
+		}
+
+		return { success: true };
 	} catch (error) {
 		console.error('Failed to add company', error);
-		return {
-			_prevState,
-			success: false,
-			errors: { form: (error as Error).message },
-		};
+		return { error: 'Failed to create company. Please try again.' };
 	}
 }
