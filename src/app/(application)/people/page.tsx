953bddcdfb5c0bf5d0f962e2f43883484c2b47a9
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-28 20:51:02 -0600

    Added session and swapped getPeople for getPeopleForCompany

--- src/app/(application)/people/page.tsx
+++ src/app/(application)/people/page.tsx
@@ -1,15 +1,21 @@
+import { auth } from '@/auth';
 import { PeopleTable } from '@/components/custom/tables/people/table';
-import { getPeople } from '@/lib/actions/people/actions';
+import { getPeopleForCompany } from '@/lib/actions/people/actions';
+import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 
 export default async function PeoplePage() {
-	const allPeople = await getPeople();
-	if (!allPeople) {
+	const session = await auth();
+	if (!session) {
+		redirect('/login');
+	}
+	const people = await getPeopleForCompany(session.user.companyId);
+	if (!people) {
 		return <div>Failed to load people</div>;
 	}
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
-			<PeopleTable people={allPeople} />
+			<PeopleTable people={people} />
 		</Suspense>
 	);
 }
