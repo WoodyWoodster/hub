77b7d8987a0c75c09a42e29914208f683443e925
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-29 21:21:39 -0600

    Added redirect to dashboard for authenticated users on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,11 @@
+import { auth } from '@/auth';
 import LogInForm from '@/components/custom/forms/auth/log-in-form';
+import { redirect } from 'next/navigation';
 
-export default function LogIn() {
+export default async function LogIn() {
+	const session = await auth();
+	if (session) {
+		redirect('/');
+	}
 	return <LogInForm />;
 }
