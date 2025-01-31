a530bd71597a890ecd8f77e13c15d9539d016147
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-31 17:13:07 -0600

    Moved getPeopleForCompany

--- src/lib/queries/roles/queries.ts
+++ src/lib/queries/roles/queries.ts
@@ -1,8 +1,8 @@
 'use server';
 
 import { db } from '@/db';
-import { roles } from '@/db/schema';
-import { sql } from 'drizzle-orm';
+import { companyPeople, companyPersonRoles, people, roles } from '@/db/schema';
+import { sql, eq } from 'drizzle-orm';
 
 export const getExternalRoles = async () => {
 	return await db
@@ -10,4 +10,29 @@ export const getExternalRoles = async () => {
 		.from(roles)
 		.where(sql`is_internal = false`)
 		.orderBy(roles.name);
+};
+
+/**
+ *
+ * @param companyId The id of the company to get people for
+ * @returns A list of people for the company
+ */
+export const getPeopleForCompany = async (companyId: string) => {
+	return await db
+		.select({
+			id: people.id,
+			fullName: people.fullName,
+			email: people.email,
+			role: roles.name,
+			dateOfBirth: people.dateOfBirth,
+			hireDate: companyPeople.hireDate,
+		})
+		.from(people)
+		.innerJoin(companyPeople, eq(people.id, companyPeople.personId))
+		.innerJoin(
+			companyPersonRoles,
+			eq(companyPeople.id, companyPersonRoles.companyPersonId),
+		)
+		.innerJoin(roles, eq(companyPersonRoles.roleId, roles.id))
+		.where(eq(companyPeople.companyId, companyId));
 };
