c8ea5ede599a93a107825dbd54428bbfa78e9407
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-02-02 14:24:00 -0600

    Created createHraPlan

---
+++ src/lib/services/hraPlanService.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { createHraPlanAction } from '../actions/hraPlans/actions';
+import { planSetupSchema } from '../schemas/onboarding/plan-setup-schema';
+
+export async function createHraPlan(data: z.infer<typeof planSetupSchema>) {
+	const validatedFields = planSetupSchema.safeParse(data);
+
+	if (!validatedFields.success) {
+		return { error: validatedFields.error.flatten().fieldErrors };
+	}
+
+	const { plan } = validatedFields.data;
+
+	try {
+		return await createHraPlanAction({
+			plan,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
