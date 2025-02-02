7358520bc30fff08b6aa13ce92adda4906aebfec
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-02-02 15:23:04 -0600

    Simplified people page

--- src/app/(application)/people/page.tsx
+++ src/app/(application)/people/page.tsx
@@ -1,28 +1,5 @@
-import { auth } from '@/auth';
 import { PeopleTable } from '@/components/custom/tables/people/table';
-import {
-	getExternalRoles,
-	getPeopleForCompany,
-} from '@/lib/queries/roles/queries';
-import { redirect } from 'next/navigation';
-import { Suspense } from 'react';
 
 export default async function PeoplePage() {
-	const session = await auth();
-	if (!session) {
-		redirect('/login');
-	}
-	const people = await getPeopleForCompany(session.user.companyId);
-	if (!people) {
-		return <div>Failed to load people</div>;
-	}
-	const roles = await getExternalRoles();
-	if (!roles) {
-		return <div>Failed to load roles</div>;
-	}
-	return (
-		<Suspense>
-			<PeopleTable people={people} roles={roles} />
-		</Suspense>
-	);
+	return <PeopleTable />;
 }
