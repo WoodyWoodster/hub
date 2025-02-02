3347554ed42c9c997f83b68363bfb686db5a302f
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-02-01 23:24:06 -0600

    Update stripeService.ts

--- src/lib/services/stripeService.ts
+++ src/lib/services/stripeService.ts
@@ -1,8 +1,8 @@
-import { createStripeCustomer as createStripeCustomerOriginal } from '@/lib/actions/stripe/actions';
+import { createStripeCustomerAction } from '@/lib/actions/stripe/actions';
 
 export async function createStripeCustomer(email: string, companyName: string) {
 	try {
-		return await createStripeCustomerOriginal(email, companyName);
+		return await createStripeCustomerAction(email, companyName);
 	} catch (error) {
 		console.error('Failed to create Stripe customer', error);
 		throw new Error('Failed to create Stripe customer');
