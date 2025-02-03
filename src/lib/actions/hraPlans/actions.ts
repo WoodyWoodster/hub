1373b0491da473692421204a802c460c92882460
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-02-02 21:00:04 -0600

    Set planSetup as completed on form submit

--- src/lib/actions/hraPlans/actions.ts
+++ src/lib/actions/hraPlans/actions.ts
@@ -1,6 +1,11 @@
 import { db } from '@/db';
-import { hraPlans } from '@/db/schema';
+import {
+	companyOnboardingProgress,
+	hraPlans,
+	onboardingSteps,
+} from '@/db/schema';
 import { PlanSetupValues } from '@/lib/schemas/onboarding/plan-setup-schema';
+import { sql } from 'drizzle-orm';
 
 export async function createHraPlanAction({ plan }: PlanSetupValues) {
 	console.log('Processing HRA plan creation');
@@ -13,6 +18,18 @@ export async function createHraPlanAction({ plan }: PlanSetupValues) {
 					startDate: plan.startDate,
 				})
 				.returning({ id: hraPlans.id });
+
+			const [planSetupStep] = await tx
+				.select({ id: onboardingSteps.id })
+				.from(onboardingSteps)
+				.where(sql`${onboardingSteps.stepName} = 'Plan Setup'`);
+
+			await tx
+				.update(companyOnboardingProgress)
+				.set({ isCompleted: true })
+				.where(
+					sql`${companyOnboardingProgress.companyId} = ${plan.companyId} AND ${companyOnboardingProgress.stepId} = ${planSetupStep.id}`,
+				);
 		});
 		return { success: true };
 	} catch (error) {
